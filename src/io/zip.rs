f9309ac8e17d168feff1235152975b9a1fa278d6
frheault <francois.m.rheault@usherbrooke.ca>
2026-08-07 15:37:17 -0400

    Clippy check for CI

--- src/io/zip.rs
+++ src/io/zip.rs
@@ -116,7 +116,7 @@ fn load_zip_offsets(
 ) -> Result<MmapBacking> {
     match dtype {
         crate::dtype::DType::UInt64 => {
-            if entry.size() % 8 != 0 {
+            if !entry.size().is_multiple_of(8) {
                 return Err(TrxError::Format(format!(
                     "offsets entry size {} is not a multiple of 8 for uint64",
                     entry.size()
@@ -159,7 +159,7 @@ fn load_zip_offsets(
             }
         }
         crate::dtype::DType::UInt32 => {
-            if entry.size() % 4 != 0 {
+            if !entry.size().is_multiple_of(4) {
                 return Err(TrxError::Format(format!(
                     "offsets entry size {} is not a multiple of 4 for uint32",
                     entry.size()
